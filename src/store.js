f5a53c113ffb3b054a0f52dcb00f2e850eda9c68
Nikita Stroganov <findoss@yandex.ru>
2022-05-19 01:08:01 +0300

    sum

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { cafeMenu } from "./menu.js";
+import { cafeMenu, cafePrice } from "./menu.js";
 
 export const store = {
   poll: {
@@ -16,7 +16,6 @@ export const store = {
   },
 
   addAnswer(payload) {
-    console.log(payload);
     this.poll.pollAnswers.push(payload);
   },
 
@@ -35,12 +34,50 @@ export const store = {
   },
 
   getLastOrderText() {
-    const textUserOrder = formatUserOrder(this.orders.at(-1).users);
-    const textComnboCount = formatComnboCount(this.orders.at(-1).users);
-    const text = `***Голодные, ваши заказы такие***\n\n${textUserOrder}\n***Общий заказ***\n\n${textComnboCount}`;
+    const lastOrder = this.orders.at(-1).users;
+    const textUserOrder = formatUserOrder(lastOrder);
+    const textComnboCount = formatComnboCount(lastOrder);
+    let text = `***Голодные, ваши заказы такие***\n\n`;
+    text += `${textUserOrder}`;
+    text += `\n***Общий заказ***${sumOrder(lastOrder)}\n\n`;
+    text += `${textComnboCount}`;
 
     return text;
   },
+};
+
+const getCombo = (users) => {
+  const combo = {};
+
+  users.forEach((user) => {
+    const idCombo = user.options.join("_");
+    if (combo.hasOwnProperty(idCombo)) {
+      combo[idCombo] += 1;
+    } else {
+      combo[idCombo] = 1;
+    }
+  });
+
+  return combo;
+};
+
+const sumOrder = (users) => {
+  const combo = getCombo(users);
+
+  const comboPrice = {};
+  cafePrice.forEach((combo) => {
+    const idCombo = combo.combo.join("_");
+    if (!comboPrice.hasOwnProperty(idCombo)) {
+      comboPrice[idCombo] = combo.price;
+    }
+  });
+
+  const sum = Object.entries(combo).reduce((acc, [k, v]) => {
+    acc += comboPrice[k] * v;
+    return acc;
+  }, 0);
+
+  return sum;
 };
 
 const formatUserOrder = (users) => {
@@ -53,16 +90,7 @@ const formatUserOrder = (users) => {
 };
 
 const formatComnboCount = (users) => {
-  const combo = {};
-
-  users.forEach((user) => {
-    const idCombo = user.options.join("_");
-    if (combo.hasOwnProperty(idCombo)) {
-      combo[idCombo] += 1;
-    } else {
-      combo[idCombo] = 1;
-    }
-  });
+  const combo = getCombo(users);
 
   return Object.entries(combo).reduce((acc, [k, v]) => {
     acc += k
